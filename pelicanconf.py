16041b01c9f40aaf2a86e9ae840e158b7ba9f490
Andrew Picciano <andrew.picciano@gmail.com>
2022-07-21 19:33:28 -0400

    Add files via upload
    to run website

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+# AUTHOR = 'andrew'
+# SITENAME = 'PyRVA'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+""" LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+"""
+
+# Social widget
+SOCIAL = (('https://twitter.com/pyrva'), ('https://github.com/PyRVA'), ('https://www.meetup.com/PyRVAUserGroup/'),('https://discord.com/invite/fSGW7Jra4T'),
+          )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
